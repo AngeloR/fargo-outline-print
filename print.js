632d0bd5e6234a12f5b7400c6e4040f4b476c1f5
Angelo R <angelo@airpost.io>
2013-07-01 16:21:39 -0700

    added ability to create a new window

--- print.js
+++ print.js
@@ -1,15 +1,27 @@
 var FargoOutlinePrint = {
 	html: {},
 	css: {},
-	getRawText: function(node) {
-		// get the text of a node and all its children
-	},
+	print: function() {
+		// get the selected node
+		var tab = getActiveTab();
+		var selectedNode = $('#' + tab.idOutlineContainer).find('.concord-cursor.selected');
+
+		// create a new window and laod the text
+		FargoOutlinePrint.window = window.open('http://example.com', '_blank', 'width=500,height=400,resizable=1');
+		FargoOutlinePrint.window.document.write(selectedNode.html());
+	}
 	init: function() {
 		// load any html views
 		// laod the css
 		
 		// insert the print icon
-		$('#idFileMenu').find('.divider').eq(0).before('<li><a href="#">Print</a></li>');
+		$('#idFileMenu').find('.divider').eq(0).before('<li><a href="#" id="print-preview">Print Preview</a></li>');
+
+		// bind to the print icon
+		$('#idFileMenu').on('click', '#print-preview', function(e) {
+			e.preventDefault();
+			FargoOutlinePrint.print();
+		});
 	}
 };
 
